fb1e5d891b35826e538d810a2e3d510a02c86dcd
Bellybrooks <ellejbrooks06@gmail.com>
2023-11-03 13:19:50 -0700

    done

--- task1.py
+++ task1.py
@@ -1,6 +1,11 @@
 def A():
+    print("this is function A. ")
 
 def B():
-
+    print("this is function B. ")
 
 def C():
+    print("this is function C. ")
+A()
+B()
+C()
--- task2.py
+++ task2.py
@@ -8,3 +8,20 @@ displays instructions and how to play
 game()
 plays the games
 """
+import random
+def number_guessing_game():
+    secret_number=random.randint(1,100)
+    attempts=0
+
+    while True:
+        guess=int(input("guess a number between 1 and 100:"))
+        attempts+=1
+        if guess<secret_number:
+            print("too low!")
+        elif guess> secret_number:
+            print("too hight!")
+        else:
+            print("congratulations! you guessed the number in",attempts,"attempts.")
+            break
+number_guessing_game()
+        
